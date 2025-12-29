bf43aab3433f907124b52b0e29a2469ad447f411
Aditya Pandey <aditya12bone@gmail.com>
2025-12-30 00:13:19 +0530

    Update apiSlice to use dynamic base URL and add Admin tag

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,9 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseQuery: '' });
+const BASE_URL =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:5000'
+    : 'https://assignment-ums-1.onrender.com';
 
-export const apiSlice = createApi({ 
-    baseQuery: baseQuery,
-    tagTypes: ['User'],
-    endpoints: (builder) => ( {} )
+export const apiSlice = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    credentials: 'include',
+  }),
+  tagTypes: ['User', 'Admin'],
+  endpoints: () => ({}),
 });
